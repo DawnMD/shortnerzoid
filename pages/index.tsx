823420c6d2bc4bc5baf4748339c62d6b25de6137
Mainak Das <mainakdas104@gmail.com>
2022-06-23 23:02:52 +0530

    Feat: Design input, add form control

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,38 @@
 import type { NextPage } from 'next';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type UrlInput = {
+  url: string;
+};
 
 const Home: NextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<UrlInput>();
+  const onSubmit: SubmitHandler<UrlInput> = (data) => console.log(data);
+
   return (
-    <div className='grid min-h-screen bg-slate-900 place-content-center'>
-      <form>
+    <div className='grid w-screen min-h-screen bg-black place-content-center'>
+      <form
+        className='flex flex-col gap-8 md:flex-row md:items-center'
+        onSubmit={handleSubmit(onSubmit)}>
         <section>
           <input
             type='text'
-            name='url'
-            id='url'
+            {...register('url')}
             placeholder='Enter Url'
-            className='rounded'
+            className='rounded-lg group focus:ring-0 focus:outline-none focus:border-0 w-80 lg:w-96'
           />
         </section>
+        <div className='relative group'>
+          <div className='animate-tilt absolute -inset-0.5 bg-gradient-to-r from-pink-600 to-purple-600 rounded-lg blur opacity-60 group-hover:opacity-100 transition duration-1000 group-hover:duration-200'></div>
+          <button className='relative w-full px-16 py-2 font-semibold tracking-wide text-pink-600 transition duration-1000 bg-black rounded-lg group-hover:duration-200 group-hover:text-purple-600'>
+            Short It!
+          </button>
+        </div>
       </form>
     </div>
   );
