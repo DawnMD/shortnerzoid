8a7af1333cd08a3a5d64824377cb1cd4eed65407
Mainak Das <mainakdas104@gmail.com>
2022-06-25 15:55:29 +0530

    Add console logs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -11,10 +11,12 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
   const slug = req.nextUrl.pathname.split('/').pop();
 
   const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-link/${slug}`);
+  console.log(slugFetch);
   if (slugFetch.status === 404) {
     return NextResponse.redirect(req.nextUrl.origin);
   }
   const data = await slugFetch.json();
+  console.log(data);
 
   if (data?.url) {
     return NextResponse.redirect(data.url);
