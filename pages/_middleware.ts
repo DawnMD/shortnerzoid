00ba0fb9361a762dd33078f81017fff0a0a824e0
Mainak Das <mainakdas104@gmail.com>
2022-06-26 11:48:46 +0530

    Disable some line

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -15,12 +15,12 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
 
   console.log(slugFetch);
 
-  if (slugFetch.status === 404) {
-    return NextResponse.redirect(req.nextUrl.origin);
-  }
-  const data = await slugFetch.json();
+  // if (slugFetch.status === 404) {
+  //   return NextResponse.redirect(req.nextUrl.origin);
+  // }
+  // const data = await slugFetch.json();
 
-  if (data?.url) {
-    return NextResponse.redirect(data.url);
-  }
+  // if (data?.url) {
+  //   return NextResponse.redirect(data.url);
+  // }
 }
