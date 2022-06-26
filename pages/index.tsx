1df8832aa5dcce5e2bff5a40b12a905bdc654819
Mainak Das <mainakdas104@gmail.com>
2022-06-26 12:18:19 +0530

    I dunno what

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { useEffect, useState } from 'react';
-import create from '../api/create';
+import create from '../endpoint/create';
 
 type UrlInput = {
   url: string;
